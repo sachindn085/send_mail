bf269707d0945eb85d8cd70b5386c71e9ab173c8
sachindn085 <sachin.dn@vaisesika.in>
2025-07-04 14:28:58 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -66,4 +66,6 @@ def send_emails():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
